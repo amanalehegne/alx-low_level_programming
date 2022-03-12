09cf30b135c608a45597f94cb671dbe0083e5fb6
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-12 10:44:19 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,40 +7,28 @@
 
 int main(void)
 {
-int ones = '0';
-int tens = '0';
-int tens2 = '0';
-int ones2 = '0';
+	int i, j;
 
-for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
-{
-	for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
+	for (i = 0; i < 100; i++)
 	{
-		for (tens2 = '0'; tens2 <= '9'; tens2++)/* prints tens digit*/
+		for (j = 0; j < 100; j++)
 		{
-			for (ones2 = '0'; ones2 <= '9'; ones2++)/* prints ones digit*/
+			if (i < j)
 			{
-				if ((((ones == 0) &&
-				      (ones2 == 0)) ||
-				     (ones < ones2)) &&
-				    (tens <= tens2))
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
-					putchar(tens);
-					putchar(ones);
+					putchar(',');
 					putchar(' ');
-					putchar(tens2);
-					putchar(ones2);
-					if (!(ones == '8' && tens == '9'
-						    ))/*addes comma and space*/
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
+
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
