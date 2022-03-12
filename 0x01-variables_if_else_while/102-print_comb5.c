d78538d4b3df7289015724884b3082cea859f434
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-12 10:42:16 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - prints all possible different combinations of two digits.
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i, j;
+int ones = '0';
+int tens = '0';
+int tens2 = '0';
+int ones2 = '0';
 
-	for (i = 0; i < 100; i++)
+for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
+{
+	for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 	{
-		for (j = 0; j < 100; j++)
+		for (tens2 = '0'; tens2 <= '9'; tens2++)/* prints tens digit*/
 		{
-			if (i < j)
+			for (ones2 = '0'; ones2 <= '9'; ones2++)/* prints ones digit*/
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if ((((ones == 0) &&
+				      (ones2 == 0)) ||
+				     (ones < ones2)) &&
+				    (tens <= tens2))
 				{
-					putchar(',');
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
+					putchar(tens2);
+					putchar(ones2);
+					if (!(ones == '8' && tens == '9'
+						    ))/*addes comma and space*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
-
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
