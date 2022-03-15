2252a31f7042cc4934fc043b303ac699cd27ad8e
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-15 19:36:04 +0300

    Updates

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,25 +5,26 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
-
-int natural(int n);
-
 int main(void)
 {
-    int sum = natural(10);
-    printf("%d", sum);
-    
-    return 0;
-}
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-int natural(int n){
-    int sum = 0;
-    for(int i = 1; i < 10; i++){
-        if(i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-            
-    }
-    return sum;
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
+	{
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
+	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
