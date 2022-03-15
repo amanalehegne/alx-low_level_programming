4572056426341f06743ef34a810478eee0492180
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-15 11:38:52 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,12 @@
  * Return: Always 0 (Success)
  */
 
-int main()
-{
-    
+int main(void)
+{  
     printf("%s %lu %s\n", "Size of a char:", sizeof(char), "byte(s)");
     printf("%s %lu %s\n", "Size of an int:", sizeof(int), "byte(s)");
     printf("%s %lu %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
     printf("%s %lu %s\n", "Size of a long long int:", sizeof(long long int), "byte(s)");
     printf("%s %lu %s\n", "Size of a float:", sizeof(float), "byte(s)");
-
     return 0;
 }
