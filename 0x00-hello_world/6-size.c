3f8eba7f07a3d4de228aeeb673e3ab8f4b64a6fb
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-15 11:40:30 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
-{  
-    printf("%s %lu %s\n", "Size of a char:", sizeof(char), "byte(s)");
-    printf("%s %lu %s\n", "Size of an int:", sizeof(int), "byte(s)");
-    printf("%s %lu %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
-    printf("%s %lu %s\n", "Size of a long long int:", sizeof(long long int), "byte(s)");
-    printf("%s %lu %s\n", "Size of a float:", sizeof(float), "byte(s)");
-    return 0;
+{
+	printf("%s %lu %s\n", "Size of a char:", sizeof(char), "byte(s)");
+	printf("%s %lu %s\n", "Size of an int:", sizeof(int), "byte(s)");
+	printf("%s %lu %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
+	printf("%s %lu %s\n", "Size of a long long int:", sizeof(long long int), "byte(s)");
+	printf("%s %lu %s\n", "Size of a float:", sizeof(float), "byte(s)");
+	return (0);
 }
