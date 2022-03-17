adc6dae24810a2c12ca267d49e0cf071523d9b7e
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-17 14:33:41 +0300

    Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print largest prime factor of 612852475143
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long num = 612852475143;
+	long factor = 2;
+
+	while (factor < num)
+	{
+		if (num % factor == 0)
+		{
+			num /= factor;
+			factor = 2;
+		}
+		else
+			factor++;
+	}
+	printf("%lu\n", num);
+	return (0);
+}
