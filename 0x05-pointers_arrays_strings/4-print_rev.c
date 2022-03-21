1353a77bfb0f1c4d871d60cd70c16d34e311690c
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-03-21 14:51:42 +0300

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse order followed by new line
+ * @s: the string
+ */
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	for (length = 0; *s != '\0'; length++)
+		s++;
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
