cfd76c968bf3006620071467802a6bdbc4b1ee08
amanalehegne <88857179+amanalehegne@users.noreply.github.com>
2022-12-20 18:49:27 +0300

    Create Word_Order.py

---
+++ Word_Order.py
@@ -0,0 +1,16 @@
+def word_order(words):
+    dic = dict()
+    for i in words:
+        dic[i] = 1 + dic.get(i, 0)
+    ans = list(dic.values())
+    print(len(ans))
+    print(ans)
+
+
+x = int(input())
+words = []
+for i in range(x):
+    words.append(input())
+
+word_order(words)
+
